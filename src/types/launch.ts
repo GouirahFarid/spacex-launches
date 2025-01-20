86e616903fd9e7d648a491d6e6eb71e0a57cb762
Your Name <you@example.com>
2025-01-20 21:02:21 +0100

    create Launch

---
+++ src/types/launch.ts
@@ -0,0 +1,37 @@
+export interface Launch {
+    id: string;
+    name: string;
+    date_utc: string;
+    date_local: string;
+    success: boolean;
+    details: string | null;
+    links: {
+        patch: {
+            small: string | null;
+            large: string | null;
+        };
+        webcast: string | null;
+        article: string | null;
+    };
+    launchpad: string;
+    payloads: string[];
+    failures: {
+        time: number;
+        altitude: number | null;
+        reason: string;
+    }[];
+}
+
+export interface Payload {
+    id: string;
+    name: string;
+    customers: string[];
+}
+
+export interface Launchpad {
+    id: string;
+    name: string;
+    full_name: string;
+}
+
+export type LaunchFilter = 'all' | 'success' | 'failed';
